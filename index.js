4a945ca3c2f57767771b473307ed369860f1eee3
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2026-01-17 13:20:07 +0700

    menggunakan api yang berasal dari node js untuk mengakses file menggunakan file system / fs
    disini penggunaan api menggunakan callback function
    
    1. readFile
    membaca data yang ada di suatu file
    2. writeFile
    memasukkan/menuliskan data pada suatu file
    3. rename
    mengubah nama suatu file

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-const { format, addDays, subDays } = require("date-fns");
+const fileSystem = require("fs");
 
-const now = new Date();
-console.log(`Today is : ${format(now, "do MMMM yyyy")}`);
+// fs.readFile() => membaca file
+fileSystem.readFile("example.txt", "utf8", (err, data) => {
+  // ketika error terjadi
+  if (err) {
+    console.log("Error reading the file:", err);
+    return;
+  }
 
-const nextWeek = addDays(now, 7);
-console.log(`Next week is : ${format(nextWeek, "do MMMM yyyy")}`);
+  // ketika berhasil membaca file
+  console.log("Successful read file\nFile contains:\n", data);
+});
 
-const previousWeek = subDays(now, 7);
-console.log(`Previous week is : ${format(previousWeek, "do MMMM yyyy")}`);
+// content untuk menulis di file
+const content = "Replace old content in example.txt";
+
+// fs.writeFile() => menulis pada suatu file
+fileSystem.writeFile("example.txt", content, (err) => {
+  // ketika error terjadi
+  if (err) {
+    console.log("Error writing to a file:", err);
+    return;
+  }
+
+  // ketika berhasil menulis file
+  console.log("File written Successfully");
+});
+
+// me-rename nama file
+fileSystem.rename("example.txt", "new_name.txt", (err) => {
+  // ketika error terjadi
+  if (err) {
+    console.log("Error renaming the file:", err);
+    return;
+  }
+
+  // ketika berhasil rename nama file
+  console.log("File renamed successfully");
+});
