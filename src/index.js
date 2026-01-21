5dd9b62b0da477e957d2a6fb06bcd03945574163
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2026-01-21 09:39:10 +0700

    mencoba menggunakan route berbeda tiap request dengan http method yang berbeda. 1.get 2.post 3.patch 4.put 5.delete

--- src/index.js
+++ src/index.js
@@ -4,12 +4,32 @@ const app = express(); // an app object / an express app
 // that app need to be passed a port number to listen port number in machine
 const port = 3001; // range port number: 0 to 65535, http://localhost:3001/
 
-// get route
-app.get("/:category", (req, res) => {
-  console.log("Request URL:", req.url);
-  console.log("Request Params:", req.params);
-  console.log("Request Query:", req.query);
-  console.log("Request Header:", req.headers);
+// GET route
+app.get("/", (req, res) => {
+  console.log("Request Method:", req.method);
+  res.send("Hello World!");
+});
+
+// POST route
+app.post("/", (req, res) => {
+  console.log("Request Method:", req.method);
+  res.send("Hello World!");
+});
+
+// PATCH route
+app.patch("/", (req, res) => {
+  console.log("Request Method:", req.method);
+  res.send("Hello World!");
+});
+
+// PUT route
+app.put("/", (req, res) => {
+  console.log("Request Method:", req.method);
+  res.send("Hello World!");
+});
+
+// DELETE route
+app.delete("/", (req, res) => {
   console.log("Request Method:", req.method);
   res.send("Hello World!");
 });
