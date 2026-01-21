dab8beee678f3464e94a162d206dbb0a55f690ed
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2026-01-21 10:18:48 +0700

    coba untuk penggunaan url params dan query parameter.
    deklrasi url parameter harus diawali dengan :  saat routing.
    ex: "/users/:role/"
    
    deklarasi query parameter bersifat opsional sesuai kebutuhan, diawali dengan ?, jika lebih dari satu dihubungkan dengan &
    ex: /?order=asc&limit=10&page=1

--- src/index.js
+++ src/index.js
@@ -4,33 +4,17 @@ const app = express(); // an app object / an express app
 // that app need to be passed a port number to listen port number in machine
 const port = 3001; // range port number: 0 to 65535, http://localhost:3001/
 
-// GET route
-app.get("/", (req, res) => {
-  console.log("Request Method:", req.method);
+// when we declare url paramaters, we use colon ( : ) to start with, ex: /:category
+app.get("/users/:role/", (req, res) => {
+  console.log("Request Params:", req.params);
+  console.log("Request Query:", req.query);
   res.send("Hello World!");
 });
 
-// POST route
-app.post("/", (req, res) => {
-  console.log("Request Method:", req.method);
-  res.send("Hello World!");
-});
-
-// PATCH route
-app.patch("/", (req, res) => {
-  console.log("Request Method:", req.method);
-  res.send("Hello World!");
-});
-
-// PUT route
-app.put("/", (req, res) => {
-  console.log("Request Method:", req.method);
-  res.send("Hello World!");
-});
-
-// DELETE route
-app.delete("/", (req, res) => {
-  console.log("Request Method:", req.method);
+// jika ingin membuat request namun url parameters nya bersifat opsional/sementara
+// gunakan question mark ( ? ),ex: /:users?
+app.get("/tasks/:users?", (req, res) => {
+  console.log("Request Params:", req.params);
   res.send("Hello World!");
 });
 
