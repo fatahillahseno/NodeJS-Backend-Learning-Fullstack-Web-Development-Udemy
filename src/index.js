66c846ca13a3def8cdebb7a0eacc9887d5c4d32e
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2026-01-26 19:42:49 +0700

    membuat middleware custom yang berisi appname dan author bernama middleWare di index.js
    lalu mencoba menggunakan informasi yang ada pada middleware tersebut di tasksRouter

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ const app = express();
 const port = 3001;
 const tasksRouter = require("./tasks/tasks.router.js");
 
+// middleware
+const middleWare = function (req, res, next) {
+  req.info = {
+    appname: "Tasks Manager",
+    author: "Sense",
+  };
+  next();
+};
+
+app.use(middleWare);
+
+// defines routes
 app.use("/", tasksRouter);
 
 app.listen(port, () => {
--- src/tasks/tasks.router.js
+++ src/tasks/tasks.router.js
@@ -2,7 +2,10 @@ const express = require("express");
 
 const tasksRouter = express.Router();
 
-tasksRouter.get("/tasks", (req, res) => res.send("All Tasks"));
+tasksRouter.get("/tasks", (req, res) => {
+  console.log(req.info);
+  return res.send(req.info);
+});
 tasksRouter.post("/tasks", (req, res) => res.send("Created Tasks"));
 
 module.exports = tasksRouter;
