1da1b7fb51647302e86b2e648abd85de3ec83368
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2026-01-27 14:04:41 +0700

    modul path dan file system, serta middleware morgan dapat membuat file logging.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+
+// route
 const tasksRouter = require("./tasks/tasks.router.js");
+
+// untuk logging
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+
+// middleware untuk mencatat logging request, saat endpoints diakses
+let accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "..", "access.log"),
+  {
+    flags: "a",
+  },
+);
+app.use(morgan("combined", { stream: accessLogStream }));
 
 // middleware untuk membaca json request
 app.use(express.json());
