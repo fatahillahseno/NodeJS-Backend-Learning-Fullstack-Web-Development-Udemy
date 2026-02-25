7269284954bb2370bfae6f768ad582ff4b933e72
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2026-02-25 08:39:17 +0700

    data request yang masuk akan di-validasi terlebih dahulu menggunakan matchedData, jika cocok dengan model schema, maka field/property data yang sama akan disimpan.

--- src/tasks/providers/createTask.provider.js
+++ src/tasks/providers/createTask.provider.js
@@ -1,16 +1,12 @@
 const Task = require("../task.schema.js");
+const { matchedData } = require("express-validator");
 
 async function createTaskProvider(req) {
-  // buat task
-  const task = new Task({
-    title: req.body.title,
-    description: req.body.description,
-    status: req.body.status,
-    priority: req.body.priority,
-    dueDate: req.body.dueDate,
-  });
+  const validatedTaskData = matchedData(req);
+  // validasi data yang telah di validasi oleh express-validator, jika ada data yang tidak valid maka data tersebut akan dihapus dari validatedTaskData
 
-  // tunggu sampai data tersimpan ke database
+  const task = new Task(validatedTaskData);
+  // membuat instance baru dari model Task dengan data yang telah divalidasi, kemudian menyimpan task tersebut ke database dan mengembalikan hasilnya
   return await task.save();
 }
 
