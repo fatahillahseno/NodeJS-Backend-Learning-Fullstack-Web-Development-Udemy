345cd4bc4c15c8895b7452b7cb7581a87c1ba688
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2026-03-06 09:30:56 +0700

    membuat createUser.validator.js
    konfigurasi users.router.js
    
    konfigurasi createUser.provider.js
    
    konfigurasi users.controller.js

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -1,9 +1,17 @@
 const express = require("express");
-
 const usersRouter = express.Router();
-
 const usersController = require("./users.controller.js");
+const createUserValidator = require("./validators/createUser.validator.js");
+const { validationResult } = require("express-validator");
+const { StatusCodes } = require("http-status-codes");
 
-usersRouter.post("/create", usersController.handleCreateUser);
+usersRouter.post("/create", createUserValidator, (req, res) => {
+  const result = validationResult(req);
+  if (result.isEmpty()) {
+    return usersController.handleCreateUser(req, res);
+  } else {
+    res.status(StatusCodes.BAD_REQUEST).json(result.array());
+  }
+});
 
 module.exports = usersRouter;
